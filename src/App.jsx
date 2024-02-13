6e689ce20c4fd40a55998d83c95072f0c5d569cd
FranciscoFil <ffilipeoffg@gmail.com>
2024-02-13 12:13:26 -0300

    primeiro

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [countB, setCountB] = useState(10)
+  const [user, setUser ] = useState()
+
+  // 1- useEffect utilização
+  useEffect(() => {
+    console.log("Roda a cada renderização")
+  })
+
+  // 2 - array de dependencias
+  useEffect(() => {
+    console.log("Só roda ao incrementar valor!")
+  }, [count ])
+
+  //  3 - ARRAY DE DEP VAZIO 
+  useEffect(() => {
+    console.log(" Só executa uma vez ! ")
+  }, [])
+
+  //  4 - clean up function 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log(`O incrementador foi alterado ${count} vezes`)
+    },2000)
+    
+    return() => {
+      clearTimeout(timer)
+    }
+  }, [count])
+
+  // 5 - fetch com useEffect
+  useEffect(() => {
+    fetch("https://api.github.com/users/FranciscoFil")
+    .then((res) => res.json())
+    .then((json) => setUser(json))
+  }, [])
+
+  return ( <div className='App'>
+        <div>
+          <button onClick={() => setCount(prevCount => prevCount + 1)}>Rederizar</button>
+          <p>{count}</p>
+        </div>
+        <div>
+          <button onClick={() => setCountB(prevCount => prevCount + 1)}>Rederizar B</button>
+          <p>{countB}</p>
+        </div>
+        {user && (
+          <div>
+          <p>Dados do usuario</p>
+          <h1>nome: {user.name}</h1>
+          <p>
+            Site <a href={user.blog}>{user.blog}</a>
+          </p>
+          <img src={user.avatar_url} />
+        </div>
+        )}
+      </div>
+      
+  )
+}
+
+export default App
